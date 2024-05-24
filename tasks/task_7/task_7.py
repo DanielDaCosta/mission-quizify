dfafaa8cc45f53b541f74b6e28e11960f8d6c771
Snow Storm <daniel.pereiracosta@hotmail.com>
2024-05-24 10:58:42 -0700

    Task 7

--- tasks/task_7/task_7.py
+++ tasks/task_7/task_7.py
@@ -72,6 +72,9 @@ class QuizGenerator:
         """
         self.llm = VertexAI(
             ############# YOUR CODE HERE ############
+            model_name='gemini-pro',
+            max_output_tokens=200,
+            temperature=0.1
         )
         
     def generate_question_with_vectorstore(self):
@@ -103,6 +106,12 @@ class QuizGenerator:
         # Initialize the LLM from the 'init_llm' method if not already initialized
         # Raise an error if the vectorstore is not initialized on the class
         ############# YOUR CODE HERE ############
+        if not self.llm:
+            self.init_llm()
+
+        if not self.vectorstore:
+            st.error("Vectorstore is not initialized", icon="🚨")
+
         
         from langchain_core.runnables import RunnablePassthrough, RunnableParallel
 
@@ -110,12 +119,14 @@ class QuizGenerator:
         # Enable a Retriever using the as_retriever() method on the VectorStore object
         # HINT: Use the vectorstore as the retriever initialized on the class
         ############# YOUR CODE HERE ############
+        retriever = self.vectorstore.db.as_retriever(search_kwargs={'k': 1})
         
         ############# YOUR CODE HERE ############
         # Use the system template to create a PromptTemplate
         # HINT: Use the .from_template method on the PromptTemplate class and pass in the system template
         ############# YOUR CODE HERE ############
-        
+        prompt = PromptTemplate.from_template(self.system_template)
+
         # RunnableParallel allows Retriever to get relevant documents
         # RunnablePassthrough allows chain.invoke to send self.topic to LLM
         setup_and_retrieval = RunnableParallel(
@@ -126,6 +137,8 @@ class QuizGenerator:
         # Create a chain with the Retriever, PromptTemplate, and LLM
         # HINT: chain = RETRIEVER | PROMPT | LLM 
         ############# YOUR CODE HERE ############
+
+        chain = setup_and_retrieval | prompt | self.llm
 
         # Invoke the chain with the topic as input
         response = chain.invoke(self.topic)
@@ -141,8 +154,8 @@ if __name__ == "__main__":
     
     embed_config = {
         "model_name": "textembedding-gecko@003",
-        "project": "YOUR-PROJECT-ID-HERE",
-        "location": "us-central1"
+        "project": "heroic-climber-423918-r1",
+        "location": "us-west1"
     }
     
     screen = st.empty()
